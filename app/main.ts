0eff64bc27e790c6303c3edbdd1f9053cf6781b4
adampasz <adampasz@gmail.com>
2016-06-09 12:47:32 -0700

    comment out some libs we are not using right now

--- app/main.ts
+++ app/main.ts
@@ -1,15 +1,18 @@
 import { bootstrap } from '@angular/platform-browser-dynamic';
-import { HTTP_PROVIDERS } from '@angular/http';
-import { provide } from '@angular/core';
-import { XHRBackend } from '@angular/http';
-import { InMemoryBackendService, SEED_DATA } from 'angular2-in-memory-web-api';
-import { InMemoryDataService } from './in-memory-data.service';
+//import { HTTP_PROVIDERS } from '@angular/http';
+//import { provide } from '@angular/core';
+//import { XHRBackend } from '@angular/http';
+//import { InMemoryBackendService, SEED_DATA } from 'angular2-in-memory-web-api';
+//import { InMemoryDataService } from './in-memory-data.service';
 // import { AppComponent } from './app.component';
 import { GLComponent } from './gl.component';
 // import {enableProdMode} from '@angular/core';
 
 // enableProdMode();
   bootstrap (GLComponent);
+
+
+
 // let debug = true;
 // if (debug) {
 //   bootstrap (GLComponent);
