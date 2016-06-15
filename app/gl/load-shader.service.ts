a0137ed86cb3ade66a82d79c6c4003888eaff6a1
adampasz <adampasz@gmail.com>
2016-06-15 12:44:17 -0700

    created load-shader.service

---
+++ app/gl/load-shader.service.ts
@@ -0,0 +1,23 @@
+import { Injectable }     from '@angular/core';
+import { Http, Response } from '@angular/http';
+import { Observable }     from 'rxjs/Observable';
+
+@Injectable()
+export class LoadShaderService {
+  constructor(private http: Http) {
+
+  }
+
+  getShader(url:string) {
+    return this.http.get(url);
+  }
+
+  private handleError(error:any) {
+    // In a real world app, we might use a remote logging infrastructure
+    // We'd also dig deeper into the error to get a better message
+             let errMsg = (error.message) ? error.message :
+                   error.status ? `${error.status} - ${error.statusText}` : 'Server error';
+                       console.error(errMsg); // log to console instead
+                           return Observable.throw(errMsg);
+  }
+}
