2d0ccca4fd476e7da07f59fe5c6f52bb824ceb7e
adampasz <adampasz@gmail.com>
2016-06-21 14:02:18 -0700

    playing around with the unit test

--- app/1st.spec.ts
+++ app/1st.spec.ts
@@ -1,6 +1,24 @@
+import {MockBackend} from '@angular/http/testing';
+import {LoadShaderAsync} from './gl/services/load-shader-async';
+import {ReflectiveInjector} from '@angular/core/';
+import {provide} from '@angular/core/';
+import {BaseRequestOptions, Http} from '@angular/http';
+
+
 describe('1st tests', () => {
-    it('true is true', () => expect(true).toEqual(true));
-    it('null is not the same thing as undefined',
-         () => expect(null).not.toEqual(undefined)
-      );
+  it('true is true', () => expect(true).toEqual(true));
+  it('null is not the same thing as undefined',
+    () => expect(null).not.toEqual(undefined)
+  );
+
+
+
 });
+describe('test LoadShaderAsync', () => {
+  it('true is true', () => {
+    return expect(true).toEqual(true)
+  });
+
+});
+
+
