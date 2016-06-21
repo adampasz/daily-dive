43f34a45c227ecff50551f70f3032f6e2d59f998
adampasz <adampasz@gmail.com>
2016-06-21 14:16:01 -0700

    roll back changes to load shader service

--- app/gl/services/load-shader-async.ts
+++ app/gl/services/load-shader-async.ts
@@ -5,35 +5,10 @@ import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/observable/throw';
 
-import {ReflectiveInjector} from '@angular/core/';
-import {provide} from '@angular/core/';
-import {BaseRequestOptions} from '@angular/http';
-import {MockBackend} from '@angular/http/testing';
-
 @Injectable()
 export class LoadShaderAsync {
   constructor(private http: Http) {
 
- var http: Http = this.getInjector().get(Http);
- //http.get('request-from-mock-backend.json').subscribe((res:Response) => doSomething(res));
-
-
-  }
-
-  getInjector() {
-
-    return ReflectiveInjector.resolveAndCreate([
-      BaseRequestOptions,
-      MockBackend,
-      provide(Http, {
-        useFactory:
-        function(backend: any, defaultOptions: any) {
-          return new Http(backend, defaultOptions);
-        },
-        deps: [MockBackend, BaseRequestOptions]
-      })
-    ]);
-    //  let lsa = new LoadShaderAsync(new Http('/app/gl/sl/vs-simple.glsl'));
   }
 
   getShader(url: string): Observable<string> {
