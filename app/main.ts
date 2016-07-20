60bb9fa6051851782966845686c1595ce68e19e3
adampasz <adampasz@gmail.com>
2016-07-19 19:35:37 -0500

    pare some stuff we are not using

--- app/main.ts
+++ app/main.ts
@@ -1,27 +1,9 @@
 import { bootstrap } from '@angular/platform-browser-dynamic';
 import { HTTP_PROVIDERS } from '@angular/http';
 import { provide } from '@angular/core';
-//import { XHRBackend } from '@angular/http';
-//import { InMemoryBackendService, SEED_DATA } from 'angular2-in-memory-web-api';
-//import { InMemoryDataService } from './in-memory-data.service';
-// import { AppComponent } from './app.component';
 import { GLComponent } from './gl.component';
+
 // import {enableProdMode} from '@angular/core';
 
 // enableProdMode();
   bootstrap (GLComponent, [HTTP_PROVIDERS]);
-
-
-
-// let debug = true;
-// if (debug) {
-//   bootstrap (GLComponent);
-// } else {
-// bootstrap(AppComponent, [HTTP_PROVIDERS, provide(XHRBackend, {
-//     useClass: InMemoryBackendService
-//   }),
-//   provide(SEED_DATA, {
-//     useClass: InMemoryDataService
-//   })
-// ]);
-// }
